1a5538cc4e9ab41cdb53b555bd4ff171b266e1b5
Angel Baro <angelbaro44@gmail.com>
2020-12-23 21:34:17 -0500

    Kata Complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function digital_root(n) {
+      let arr =  n.toString().split('').map((x)=> Number(x));
+         while (arr.length > 1){
+         arr = arr.reduce((x,y)=>((y+x))).toString().split('').map((x)=> Number(x));   
+      }
+      console.log(   Number(arr.toString())  )
+      return Number(arr.toString())
+}
 
